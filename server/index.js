d457242490ca1ee5e5e0f342f87ea533532e1fca
Robert Potter <robert.potter.741@gmail.com>
2024-11-07 13:39:16 -0500

    Include loader token route resolution

--- server/index.js
+++ server/index.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT;
 
 const app = express();
 app.use(express.json());
+app.use('/loaderio-89bdd503722ff2b68d887292de4bfcf1', (req, res) => {
+  res.send('loaderio-89bdd503722ff2b68d887292de4bfcf1');
+})
 app.use('/api', router);
 
 app.listen(PORT, () => {
